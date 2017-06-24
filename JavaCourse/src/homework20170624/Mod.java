4184e5959ddd69e21e1b75e4fa9fc7a77c3cd93a
Iren <nor.iren@yandex.ru>
2017-06-24 23:37:33 +0300

    fix name of function

--- JavaCourse/src/homework20170624/Mod.java
+++ JavaCourse/src/homework20170624/Mod.java
@@ -2,7 +2,7 @@ package homework20170624;
 
 public class Mod {
 
-	public static int findMod(int a, int b) {
+	public static int mod(int a, int b) {
 		if (b == 0){
 			System.err.println("The second operand is zero, so the behavior is undefined");
 			return a;
--- JavaCourse/src/homework20170624/ModTest.java
+++ JavaCourse/src/homework20170624/ModTest.java
@@ -2,16 +2,16 @@ package homework20170624;
 
 public class ModTest {
 	public static void main(String [] args){
-		System.out.println(SimpleUnit.assertEquals(Mod.findMod(10, 6), 4));
-		System.out.println(SimpleUnit.assertNotEquals(Mod.findMod(10, 6), 6));
+		System.out.println(SimpleUnit.assertEquals(Mod.mod(10, 6), 4));
+		System.out.println(SimpleUnit.assertNotEquals(Mod.mod(10, 6), 6));
 		
-		System.out.println(SimpleUnit.assertEquals(Mod.findMod(10, 10), 0));
-		System.out.println(SimpleUnit.assertEquals(Mod.findMod(1, 10), 1));
+		System.out.println(SimpleUnit.assertEquals(Mod.mod(10, 10), 0));
+		System.out.println(SimpleUnit.assertEquals(Mod.mod(1, 10), 1));
 		
-		System.out.println(SimpleUnit.assertEquals(Mod.findMod(10, Integer.MAX_VALUE), 10));
-		System.out.println(SimpleUnit.assertEquals(Mod.findMod(-15, 26), -15));
-		System.out.println(SimpleUnit.assertEquals(Mod.findMod(-50, 11), -6));
+		System.out.println(SimpleUnit.assertEquals(Mod.mod(10, Integer.MAX_VALUE), 10));
+		System.out.println(SimpleUnit.assertEquals(Mod.mod(-15, 26), -15));
+		System.out.println(SimpleUnit.assertEquals(Mod.mod(-50, 11), -6));
 		
-		System.out.println(SimpleUnit.assertEquals(Mod.findMod(-50, 0), -50));
+		//System.out.println(SimpleUnit.assertEquals(Mod.mod(-50, 0), -50));
 	}
 }
