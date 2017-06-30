840a9396193a420e993416fcf1d207799f5d1459
Iren <nor.iren@yandex.ru>
2017-06-30 13:49:39 +0300

    add required

--- JavaCourse/src/homework20170627/NumeralSystem.java
+++ JavaCourse/src/homework20170627/NumeralSystem.java
@@ -23,5 +23,56 @@ public class NumeralSystem {
 		}
 		return r;
 	}
+	
+	public static String toBinaryString(int n){
+		String res = "";
+		int d = n % 2;
+		n = n / 2;
+		while (n > 0) {
+			res += DIGITS.charAt(d);
+			d = n % 2;
+			n = n / 2;
+		}
+		res += DIGITS.charAt(d);
+		String r = "";
+		for (int i = res.length() - 1; i >= 0; i--) {
+			r += res.charAt(i);
+		}
+		return r;
+	}
+	
+	public static String toHexString(int n){
+		String res = "";
+		int d = n % 16;
+		n = n / 16;
+		while (n > 0) {
+			res += DIGITS.charAt(d);
+			d = n % 16;
+			n = n / 16;
+		}
+		res += DIGITS.charAt(d);
+		String r = "";
+		for (int i = res.length() - 1; i >= 0; i--) {
+			r += res.charAt(i);
+		}
+		return r;
+	}
+	
+	public static String toOctalString(int n){
+		String res = "";
+		int d = n % 8;
+		n = n / 8;
+		while (n > 0) {
+			res += DIGITS.charAt(d);
+			d = n % 8;
+			n = n / 8;
+		}
+		res += DIGITS.charAt(d);
+		String r = "";
+		for (int i = res.length() - 1; i >= 0; i--) {
+			r += res.charAt(i);
+		}
+		return r;
+	}
 
 }
--- JavaCourse/src/homework20170627/TestNumeralSystem.java
+++ JavaCourse/src/homework20170627/TestNumeralSystem.java
@@ -8,6 +8,10 @@ public class TestNumeralSystem {
 		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toString(255, 2), "11111111"));
 		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toString(13, 3), "111"));
 		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toString(14031996, 8), "65416174"));
+		
+		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toBinaryString(255), "11111111"));
+		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toHexString(14), "E"));
+		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toOctalString(14031996), "65416174"));
 
 		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toString(14, 1), "error base"));
 		System.out.println(SimpleUnit.assertEquals(NumeralSystem.toString(14, 37), "error base"));
