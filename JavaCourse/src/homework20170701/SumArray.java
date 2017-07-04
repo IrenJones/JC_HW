33fe9d6e677f663fb426c5d02ab0cb56b102fdf2
Iren <nor.iren@yandex.ru>
2017-07-04 12:19:08 +0300

    fix with continue

--- JavaCourse/src/homework20170701/SumArray.java
+++ JavaCourse/src/homework20170701/SumArray.java
@@ -4,12 +4,13 @@ public class SumArray {
 
 	public static void findSum(int[][] arr) {
 		int locSum = 0;
-		for (int i = 0; i < arr.length; i++) {
+		OUTER: for (int i = 0; i < arr.length; i++) {
 			int[] row = arr[i];
 			for (int j = 0; j < row.length; j++) {
 				if (row[j] < 0){
 					locSum =0;
-					break;
+					System.out.println(locSum);
+					continue OUTER;
 				}
 				else{
 					locSum+=row[j];
