bc12b0a902ff23b5fca52a874d0b772bd7173715
Iren <nor.iren@yandex.ru>
2017-07-05 21:16:09 +0300

    add Print Array class

---
+++ JavaCourse/src/homework20170704/ArraysFunc.java
@@ -0,0 +1,15 @@
+package homework20170704;
+
+public class ArraysFunc {
+
+	public static void printString(int[] arr) {
+		for (int i=0;i<arr.length;i++){
+			System.out.print(arr[i]);
+			if(i<arr.length-1){
+				System.out.print(",");
+			}
+		}
+		System.out.println("");
+	}
+	
+}
---
+++ JavaCourse/src/homework20170704/ArraysFuncTest.java
@@ -0,0 +1,9 @@
+package homework20170704;
+
+public class ArraysFuncTest {
+	public static void main(String [] args){
+		ArraysFunc.printString(new int[]{1,2,3,4,5});
+		ArraysFunc.printString(new int[]{});
+		ArraysFunc.printString(new int[]{1});
+	}
+}
