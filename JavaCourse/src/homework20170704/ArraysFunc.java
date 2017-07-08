4cb0f8a550f2f7562cf916275151e848269608ce
Iren <nor.iren@yandex.ru>
2017-07-08 20:33:59 +0300

    upd []

--- JavaCourse/src/homework20170704/ArraysFunc.java
+++ JavaCourse/src/homework20170704/ArraysFunc.java
@@ -3,12 +3,14 @@ package homework20170704;
 public class ArraysFunc {
 
 	public static void printString(int[] arr) {
+		System.out.println("[");
 		for (int i=0;i<arr.length;i++){
 			System.out.print(arr[i]);
 			if(i<arr.length-1){
 				System.out.print(",");
 			}
 		}
+		System.out.println("]");
 		System.out.println("");
 	}
 	
