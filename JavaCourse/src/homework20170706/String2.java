d035bf7e6248645a251bcb9217b31d52f9cf3d43
Iren <nor.iren@yandex.ru>
2017-07-09 21:34:42 +0300

    upd String-2

--- JavaCourse/src/homework20170706/String2.java
+++ JavaCourse/src/homework20170706/String2.java
@@ -247,6 +247,22 @@ public class String2 {
 		return s;
 	}
 
+	public String wordEnds(String str, String word) {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < str.length() - word.length() + 1; i++) {
+			if (str.substring(i, i + word.length()).equals(word)) {
+				if (i > 0) {
+					s.append(str.charAt(i - 1));
+				}
+				if (i + word.length() < str.length()) {
+					s.append(str.charAt(i + word.length()));
+				}
+				i += word.length() - 1;
+			}
+		}
+		return s.toString();
+	}
+
 	public static void main(String[] args) {
 	}
 }
