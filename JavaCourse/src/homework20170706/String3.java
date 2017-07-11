834b170bc7a4e15df0937a87c4a6aeba156c87bb
Iren <nor.iren@yandex.ru>
2017-07-11 15:50:32 +0300

    upd String-3

--- JavaCourse/src/homework20170706/String3.java
+++ JavaCourse/src/homework20170706/String3.java
@@ -17,8 +17,41 @@ public class String3 {
 		}
 		return count;
 	}
-	
+
+	public static boolean equalIsNot(String str) {
+		int isCount = 0;
+		int notCount = 0;
+		boolean flag = false;
+		String word = "";
+		int c = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'n') {
+				word = "not";
+			} else if (str.charAt(i) == 'i') {
+				word = "is";
+				flag = true;
+			}
+			for (int j = 0; j < word.length() && i + j < str.length(); j++) {
+				if (str.charAt(i + j) == word.charAt(j)) {
+					c++;
+				}
+			}
+			if (c == word.length()) {
+				if (flag) {
+					isCount++;
+				} else if (word.length()>0){
+					notCount++;
+				}
+			}
+			c = 0;
+			flag = false;
+			word="";
+		}
+		return notCount == isCount ? true : false;
+	}
+
 	public static void main(String[] args) {
+		System.out.println(equalIsNot("This is notnot"));
 	}
 
 }
