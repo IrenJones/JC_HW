7fa41671b92a5cfb2ac843e1ee8beb1a4e43b1f3
Iren <nor.iren@yandex.ru>
2017-07-12 22:37:21 +0300

    fix comments

--- JavaCourse/src/cw20170712/OldApproach.java
+++ JavaCourse/src/cw20170712/OldApproach.java
@@ -1,8 +1,9 @@
 package cw20170712;
 
-// Процедурный подход
-// Данные отдельно - методы отдельно
-// Передавать стек мы вынуждены
+// Procedural programming
+// Data and methods are separated
+// We need to use stack as a parameter of function
+// That's why it's really useful to use OOP
 public class OldApproach {
 	static class Stack {
 		String[] elements;
