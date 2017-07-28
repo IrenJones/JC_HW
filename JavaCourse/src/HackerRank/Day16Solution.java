94e8173b48a59180f47ee624d0a4db8d3f1fc274
Iren <nor.iren@yandex.ru>
2017-07-28 21:12:32 +0300

    add Day-16 solution

---
+++ JavaCourse/src/HackerRank/Day16Solution.java
@@ -0,0 +1,17 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class Day16Solution {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try{
+            System.out.println(Integer.parseInt(S));
+        }
+        catch(Exception e){
+            System.out.println("Bad String");
+        }
+        in.close();
+    }
+}
