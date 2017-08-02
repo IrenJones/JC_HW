98aaef6796f799c2d75389001a1f5386b72e2c5d
Iren <nor.iren@yandex.ru>
2017-08-02 22:14:37 +0300

    fix

--- JavaCourse/src/homework20170724/Rat.java
+++ JavaCourse/src/homework20170724/Rat.java
@@ -1,8 +1,7 @@
 package homework20170724;
 
 public class Rat implements Pet {
-	boolean state = false;	
-	public Brain brain = new HungryRatBrainState(this);
+	Brain brain = new HungryRatBrainState(this);
 	
 	@Override
 	public void feed() {
