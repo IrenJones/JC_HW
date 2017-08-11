706c32b8ad876e52725f7f68776c7f3e9df148b1
Iren <nor.iren@yandex.ru>
2017-08-11 14:01:15 +0300

    fix name of class

--- JavaCourse/src/cw20170811/SortClassExample.java
+++ JavaCourse/src/cw20170811/SortClassExample.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class SosrtClassExample {
+public class SortClassExample {
 
 	public static void main(String[] args) {
 		
