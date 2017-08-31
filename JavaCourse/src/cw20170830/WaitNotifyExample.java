ad2c9f71651e69ddd7d3fb04f887df2d3d5a6490
Iren <nor.iren@yandex.ru>
2017-08-31 18:19:37 +0300

    add

--- JavaCourse/src/cw20170830/WaitNotifyExample.java
+++ JavaCourse/src/cw20170830/WaitNotifyExample.java
@@ -13,6 +13,7 @@ public class WaitNotifyExample {
 				System.err.println("locked");
 				try{
 					monitor.wait();
+					System.err.println("notified!");
 				}
 				catch(Exception e){
 					e.printStackTrace();
@@ -20,13 +21,21 @@ public class WaitNotifyExample {
 			}
 		});
 		
+		thread.start();
+		
+		Utils.pause(20000);
 		
 		synchronized(monitor){
-			System.out.println("start");
+			/*System.out.println("start");
 			thread.start();
 			System.out.println("sleep");
 			Utils.pause(5000);
 			System.out.println("awake");
+			*/
+			System.out.println("notify me");
+			monitor.notify();
+			System.out.println("sleeping");
+			Utils.pause(4000);
 		}
 
 	}
